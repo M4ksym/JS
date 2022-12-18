b3151034c272421cd818532f0c9d71b7e4649aa8
M4ksym <107857845+M4ksym@users.noreply.github.com>
2022-12-18 19:19:31 +0200

    Create 02-script.js

---
+++ jss/02-script.js
@@ -0,0 +1,47 @@
+// const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'];
+
+// console.table(friends);
+
+// const a = [1, 2, 3, 4];
+// const b = a;
+
+// console.table(a);
+// console.table(b);
+
+// const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'];
+
+// for (const friend of friends) {
+//   console.log(friend);
+// }
+
+// const cart = [54, 105, 28, 44, 89, 17, 70, 104];
+// let total = 0;
+
+// for (let i = 0; i < cart.length; i += 1) {
+//   console.log(cart[i]);
+//   total += cart[i];
+// }
+// console.log('total', total);
+
+const numbers = [1, 2, 6, 4, 7, 90, 11, 54, 31, 77, 12, 84, 80, 7, 99, 14];
+let total = 0;
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//   console.log(numbers[i]);
+
+//   if (numbers[i] % 2 === 0) {
+//     console.log('Четное');
+//   }
+//   total += numbers[i];
+// }
+
+for (const number of numbers) {
+  console.log(number);
+
+  if (number % 2 === 0) {
+    console.log('Четное');
+  }
+  total += number;
+}
+
+console.log('Total:', total);
