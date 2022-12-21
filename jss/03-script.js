5b9414b16fdf5e24ccc24fd02b9d32d7770a8a85
M4ksym <107857845+M4ksym@users.noreply.github.com>
2022-12-21 18:54:15 +0200

    Update 03-script.js

---
+++ jss/03-script.js
@@ -0,0 +1,36 @@
+// const add = function (a, b) {
+//   console.log(a);
+//   console.log(b);
+
+//   const result = a * b;
+//   console.log('a+b:', result);
+//   console.log('Выполняется функция add');
+// };
+// add(40, 9);
+// add(22, 4);
+// add(54, 76);
+// !!!!
+// !!!!
+// const fn = function () {
+//   console.log(1);
+//   console.log(2);
+//   console.log(3);
+
+//   return 'Fuck you';
+// };
+// console.log('Результат:', fn());
+
+// !!!!!
+// !!!!!
+const fnA = function () {
+  console.log('Выполняется функция А');
+};
+const fnB = function () {
+  console.log('Выполняется функция B');
+};
+const fnC = function () {
+  console.log('Выполняется функция C');
+};
+fnA();
+fnB();
+fnC();
