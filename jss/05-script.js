ce298b85948e79b0d82ae9e8b33ed306135af84f
M4ksym <107857845+M4ksym@users.noreply.github.com>
2022-12-27 20:24:26 +0200

    Create 05-script.js

---
+++ jss/05-script.js
@@ -0,0 +1,131 @@
+// const playlist = {
+//   name: 'My playlits',
+//   rating: 4,
+//   tracks: ['Трек-1', 'Трек-2', 'Трек-3'],
+//   trackCount: 3,
+// };
+
+// console.log(playlist);
+
+// !!!!!!
+// !!!!!
+// !!!!!!
+
+// const playlist = {
+//   name: 'My playlits',
+//   rating: 4,
+//   tracks: ['Трек-1', 'Трек-2', 'Трек-3'],
+//   trackCount: 3,
+// };
+
+// console.log(playlist.tracks);
+// console.log(playlist.name);
+// console.log(playlist.trackCount);
+
+// !!!!!
+// !!!!!
+// !!!!!
+
+// const playlist = {
+//   name: 'My playlits',
+//   rating: 4,
+//   tracks: ['Трек-1', 'Трек-2', 'Трек-3'],
+//   trackCount: 3,
+// };
+// const propertyName = 'tracks';
+
+// console.log(playlist[propertyName]);
+
+// !!!!!!
+// !!!!!
+// !!!!!
+// const username = 'Mango';
+// const email = 'mango@gmaol.com';
+
+// const signupData = {
+//   username: username,
+//   email: email,
+// };
+// console.log(signupData);
+
+// !!!!!!!
+// !!!!!!
+
+// const username = 'Mango';
+// const email = 'mango@gmaol.com';
+
+// const signupData = {
+//   username,
+//   email,
+// };
+// console.log(signupData);
+
+// !!!!!!!!
+// !!!!!!!
+
+// const inputName = 'color';
+// const inputValue = 'tomato';
+
+// const colorPickerData = {
+//   [inputName]: inputValue,
+// };
+// console.log(colorPickerData);
+
+// !!!!!!!!
+// !!!!!!!!!
+// !!!!!!!!
+
+// const a = { x: 1, y: 1 };
+// const b = a;
+
+// console.log(a === b);
+
+// 1!!!!!!
+// !!!!!!!
+// !!!!!!
+
+// const a = [1, 2, 3, 4];
+
+// a.push(5);
+
+// console.log(a);
+
+// !!!!!!
+// !!!!!!
+// !!!!!!
+
+// const playlist = {
+//   name: 'My playlist',
+//   rating: 4,
+//   tracks: ['Трек-1', 'Трек-2', 'Трек-3'],
+//   trackCount: 3,
+//   changeName(newName) {
+//     // console.log(this);
+//     this.name = newName;
+//   },
+//   addtrack(track) {
+//     this.tracks.push(track);
+//   },
+//   updateRating(newRating) {
+//     this.rating = newRating;
+//   },
+// };
+
+// playlist.changeName('New Name');
+
+// playlist.addtrack('New track');
+
+// playlist.updateRating(1);
+
+// console.log(playlist);
+
+// !!!!!!!!
+// !!!!!!!
+// !!!!!!!
+
+const feedbeck = {
+  good: 5,
+  neutral: 10,
+  bad: 99,
+};
+let totalFeedback = 0;
