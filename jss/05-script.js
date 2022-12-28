af1b80a1af88b913633a3db610b8a94e6052c71a
M4ksym <107857845+M4ksym@users.noreply.github.com>
2022-12-28 11:34:58 +0200

    Update 05-script.js

--- jss/05-script.js
+++ jss/05-script.js
@@ -258,25 +258,29 @@
 
 // console.log(getAllNames(friends));
 
-// !!!!!!
-// !!!!!
-// !!!!!
+// -------
+// -------
+// --------
 
-const cart = {
-  items: [],
-  getItems() {},
-  add(product) {},
-  remove(productName) {},
-  clear() {},
-  countTotalPrice() {},
-  increaseQuantity(productName) {},
-  decreaseQuatity(productName) {},
-};
-console.table(cart.getItems());
+// const cart = {
+//   items: [],
+//   getItems() {},
+//   add(product) {},
+//   remove(productName) {},
+//   clear() {},
+//   countTotalPrice() {},
+//   increaseQuantity(productName) {},
+//   decreaseQuatity(productName) {},
+// };
+// console.table(cart.getItems());
 
-cart.add({ name: 'apple', price: 50 });
-cart.add({ name: 'lemon', price: 70 });
-cart.add({ name: 'pie', price: 90 });
-cart.add({ name: 'watermelon', price: 95 });
+// cart.add({ name: 'apple', price: 50 });
+// cart.add({ name: 'lemon', price: 70 });
+// cart.add({ name: 'pie', price: 90 });
+// cart.add({ name: 'watermelon', price: 95 });
 
-console.table(cart.getItems());
+// console.table(cart.getItems());
+
+// -----------
+// -----------
+// -----------
