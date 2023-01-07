8c208a8b527551520eaf61b1fe805e2ae12e3f24
M4ksym <107857845+M4ksym@users.noreply.github.com>
2023-01-07 22:31:34 +0200

    Update 06-script.js

--- jss/06-script.js
+++ jss/06-script.js
@@ -1,23 +1,83 @@
+// !!
+// 1!!!
+// !!!!!
+// /1!!!!!
+
 const cart = {
   items: [],
   getItems() {
     return this.items;
   },
-  add(product) {},
-  remove(productName) {},
-  clear() {},
-  countTotalPrice() {},
+  add(product) {
+    for (const item of this.items) {
+      if (item.name === product.name) {
+        item.quantity += 1;
+        return;
+      }
+    }
+
+    const newProduct = {
+      ...product,
+      quantity: 1,
+    };
+
+    this.items.push(newProduct);
+  },
+  remove(productName) {
+    const { items } = this;
+
+    for (let i = 0; i < items.length; i += 1) {
+      const { name } = items[i];
+
+      if (productName === name) {
+        console.log('Hашли продукт -', productName);
+        console.log(i);
+
+        items.splice(i, 1);
+      }
+    }
+  },
+
+  clear() {
+    this.items = [];
+  },
+  countTotalPrice() {
+    const { items } = this;
+
+    let total = 0;
+
+    for (const { price, quantity } of items) {
+      total += price * quantity;
+    }
+
+    return total;
+  },
+
   increaseQuantity(productName) {},
   decreaseQuatity(productName) {},
 };
-console.table(cart.getItems());
+console.log(cart.getItems());
 
 cart.add({ name: 'apple', price: 50 });
+cart.add({ name: 'apple', price: 50 });
 cart.add({ name: 'lemon', price: 70 });
-cart.add({ name: 'pie', price: 95 });
+cart.add({ name: 'lemon', price: 70 });
+cart.add({ name: 'pie', price: 85 });
+cart.add({ name: 'watermelon', price: 95 });
+cart.add({ name: 'watermelon', price: 95 });
 cart.add({ name: 'watermelon', price: 95 });
 
 console.table(cart.getItems());
+
+console.log('Total:', cart.countTotalPrice());
+
+cart.remove('pie');
+console.table(cart.getItems());
+
+console.log('Total:', cart.countTotalPrice());
+
+cart.clear();
+console.log(cart.getItems());
 
 // !!!!!!!!
 // !!!!!!!!!
