cb8a8d540c36f21a7140587746b5382d010444d4
M4ksym <107857845+M4ksym@users.noreply.github.com>
2023-01-21 10:55:08 +0200

    Update 07-script.js

--- jss/07-script.js
+++ jss/07-script.js
@@ -1,3 +1,7 @@
+// !!!!
+//                          CALLBACK
+// !!
+
 // const fnA = function (message, callback) {
 //   console.log(message);
 
@@ -100,9 +104,9 @@
 
 //   for (const el of array) {
 //     console.log(el);
-//     const passet = test(el);
+//     const passed = test(el);
 
-//     if (passet) {
+//     if (passed) {
 //       filterArray.push(el);
 //     }
 //   }
@@ -111,13 +115,91 @@
 // };
 
 // const callback1 = function (value) {
-//   return value <= 3;
+//   return value >= 3;
 // };
-
 // const r1 = filter([1, 2, 3, 4, 5], callback1);
 // console.log(r1);
 
-// const r2 = filter([1, 2, 3, 4, 5, 6, 7, 8, 9], function (value) {
-//   return value >= 5;
-// });
+// const callback2 = function (value) {
+//   return value <= 5;
+// };
+// const r2 = filter([1, 2, 3, 4, 5, 6, 7, 8, 9], callback2);
 // console.log(r2);
+
+// const fruits = [
+//   { name: 'apples', quantity: 200, isFresh: true },
+//   { name: 'orange', quantity: 80, isFresh: false },
+//   { name: 'lemon', quantity: 220, isFresh: true },
+//   { name: 'grapes', quantity: 150, isFresh: false },
+//   { name: 'bananas', quantity: 100, isFresh: true },
+// ];
+
+// const getFruitsWithQuantity = function (fruit) {
+//   return fruit.quantity >= 120;
+// };
+
+// const r3 = filter(fruits, getFruitsWithQuantity);
+// console.log(r3);
+
+// !!!!!!
+// !!!!!!
+// !!!!!!
+
+//              замыкание
+
+// const fnA = function (parameter) {
+//   const innerVariable = 'значение внутринней функции fnA';
+
+//   const innerFunction = function () {
+//     console.log(parameter);
+//     console.log(innerVariable);
+//     console.log('это вызов innerVariable');
+//   };
+//   return innerFunction;
+// };
+// const fnB = fnA(333);
+
+// fnB();
+
+// console.log(fnB);
+
+// !!!!
+// !!!!
+
+// const makeDish = function (sheffName, dish) {
+//   console.log(`${sheffName} готовит ${dish}`);
+// };
+
+// makeDish('Mango', 'Пирожок');
+// makeDish('Mango', 'Омлет');
+// makeDish('Mango', 'Чай');
+
+// makeDish('Poly', 'Котлеты');
+// makeDish('Poly', 'Суп');
+// makeDish('Poly', 'Кофе');
+
+// ++++++
+// ++++++
+
+//             замыкание
+
+// const makeSheff = function (name) {
+//   const makeDish = function (dish) {
+//     console.log(`${name} готовит ${dish}`);
+//   };
+//   return makeDish;
+// };
+
+// const mango = makeSheff('Mango');
+// mango('пирожок');
+
+// !!!!!!!
+// !!!!!!!
+// 1!!!!!!!
+
+//          округлятор
+const floatingPoint = 3.45678;
+const someInt = Math.round(floatingPoint);
+comst withDecimals
+
+console.log(floatingPoint);
