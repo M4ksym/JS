ac4fa9c9c9c943b6cb77b34b4f80373b361899ac
M4ksym <107857845+M4ksym@users.noreply.github.com>
2023-01-24 23:19:30 +0200

    Update 01-script.js

--- jss/01-script.js
+++ jss/01-script.js
@@ -1,3 +1,75 @@
+// !!!!!!!
+// 1!!!!!!!
+// !!!!!!!!!
+
+// let username;
+// console.log(typeof username); // "undefined"
+
+// let inputValue = null;
+// console.log(typeof inputValue); // "object"
+
+// const quantity = 17;
+// console.log(typeof quantity); // "number"
+
+// const message = "JavaScript is awesome!";
+// console.log(typeof message); // "string"
+
+// const isSidebarOpen = false;
+// console.log(typeof isSidebarOpen); // "boolean"
+
+// !!!!!!!!
+// !!!!!!!!!
+// !!!!!!!!!!
+
+//         вызов подтверждения да или нет
+
+// const isComing = confirm('Please confirm hotel reservation');
+// console.log(isComing);
+
+// !!!!!!!!!!!!!!
+// !!!!!!!!!!!!!!!
+// 1!!!!!!!!!!!!!!
+
+//               вызов модального окна с подтверждением
+
+// const value = prompt('Please enter a number!');
+// console.log(typeof value); // "string"
+// console.log(value); // "5"
+
+// !!!!!!!!
+// !!!!!!!!
+// 1!!!!!!!
+
+// // Math.floor(num) - повертає найбільше ціле число,
+// // менше, або яке дорівнює зазначеному числу
+// console.log(Math.floor(1.7)); // 1
+
+// // Math.ceil(num) - повертає найменше ціле число,
+// // більше, або яке дорівнює зазначеному числу.
+// console.log(Math.ceil(1.2)); // 2
+
+// // Math.round(num) - повертає значення числа,
+// // округленого до найближчого цілого
+// console.log(Math.round(1.2)); // 1
+// console.log(Math.round(1.5)); // 2
+
+// // Math.max(num1, num2, ...) - повертає найбільше ціле число з набору
+// console.log(Math.max(20, 10, 50, 40)); // 50
+
+// // Math.min(num1, num2, ...) - повертає найменше ціле число з набору
+// console.log(Math.min(20, 10, 50, 40)); // 10
+
+// // Math.pow(base, exponent) - піднесення до степеня
+// console.log(Math.pow(2, 4)); // 16
+
+// // Math.random() - повертає псевдовипадкове число в діапазоні [0, 1)
+// console.log(Math.random()); // випадкове число між 0 і 1
+// console.log(Math.random() * (10 - 1) + 1); // псевдовипадкове число від 1 до 10
+
+// !!!!!!!!!
+// 1!!!!!!!!!!
+// !!!!!!!!!!
+
 // const stars = 3;
 // let price;
 
