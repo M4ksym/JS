304e662296bf9f08f0fdc6f106505dfe8381b6c0
M4ksym <107857845+M4ksym@users.noreply.github.com>
2023-03-21 18:40:31 +0200

    Update 01-script.js

--- jss/01-script.js
+++ jss/01-script.js
@@ -279,106 +279,12 @@
 
 //                           ТЕРНАРНЫЙ ОПЕРАТОР  ? :
 
-const balance = 1000;
+// const balance = 1000;
 
-let message = balance > 0 ? 'Позитивный баланс' : 'Негативный баланс';
+// let message = balance > 0 ? 'Позитивный баланс' : 'Негативный баланс';
 
-console.log(message);
+// console.log(message);
 
 //                          ТЕРНАРНЫЙ ОПЕРАТОР
 
-// !!!!!!!!!!!!
-// !!!!!!!!!!!!!
-
-//                                 УРОК НОМЕР ОДИН ЗАКОНЧЕН
-
 // console.log(11 > 10);
-
-//                                 УРОК НОМЕР ДВА ОТКРЫТ
-// const stars = 9;
-// let price;
-
-// switch (stars) {
-//   case 1:
-//   case 2:
-//     price = 20;
-//     break;
-//   case 3:
-//   case 4:
-//     price = 220;
-//     break;
-
-//   default:
-//     console.log('Много хочешь ');
-// }
-// console.log(price);
-
-// !!!!!!!!!!!!
-// !!!!!!!!!!!!!
-
-// const option = 4;
-// let message = '';
-
-// switch (option) {
-//   case 1:
-//     message = 'Вы Можете Забрать Товар из Урны';
-//     break;
-//   case 2:
-//     message = 'Черт Доставит Вам Товар';
-//     break;
-//   case 3:
-//     message = 'Вам отправят товар Укр Почтой';
-//     break;
-//   default:
-//     message = 'Вам перезвонит наш Урод';
-// }
-// console.log(message);
-
-// !!!!!!!!!!!
-// !!!!!!!!!!!!
-// !!!!!!!!!!!!!!!
-
-// let totalSpent = 3250;
-// let payment = 437;
-// let discount = 0;
-
-// if (totalSpent >= 100 && totalSpent < 1000) {
-//   console.log('Бронзовый партнёр,скидка 2%');
-//   discount = 0.02;
-// } else if (totalSpent >= 1000 && totalSpent < 3000) {
-//   console.log('Серебряный партёр ,скидка 7%');
-//   discount = 0.07;
-// } else if (totalSpent >= 3001) {
-//   console.log('Золотой партёр ,скидка 12%');
-//   discount = 0.12;
-// } else {
-//   console.log('Не партнёр, скидка 0%');
-// }
-
-// payment -= payment * discount;
-
-// console.log(
-//   `Оформляем заказ на сумму ${payment} со скидкой ${discount * 100}%`,
-// );
-
-// totalSpent += payment;
-
-// console.log(`Общая сумма потреченого в магазине ${totalSpent} `);
-
-// const bthAdd = document.querySelector('button[data-add]');
-// const valueInput = document.querySelector('input[data-value]');
-
-// let total = 0;
-
-// bthAdd.addEventListener('click', () => {
-//   console.log('click');
-
-//   const value = Number(valueInput.value);
-//   console.log(value);
-//   total += value;
-//   console.log('total:', total);
-//   valueInput.value = '';
-// });
-
-// !!!!!!!!!
-// !!!!!!!!
