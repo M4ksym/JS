86ce5e7b9d1a717719aff2ecd08ff37153767a26
M4ksym <107857845+M4ksym@users.noreply.github.com>
2023-03-22 14:37:40 +0200

    Update 02-script.js

--- jss/02-script.js
+++ jss/02-script.js
@@ -1,201 +1,122 @@
-// const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'];
-
-// console.table(friends);
-
-// const a = [1, 2, 3, 4];
-// const b = a;
-
-// console.table(a);
-// console.table(b);
-// !!!!!!
-// !!!!!!
-// !!!!!!
-// const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'];
-
-// for (const friend of friends) {
-//   console.log(friend);
-// }
-
-// const cart = [54, 105, 28, 44, 89, 17, 70, 104];
-// let total = 0;
-
-// for (let i = 0; i < cart.length; i += 1) {
-//   console.log(cart[i]);
-//   total += cart[i];
-// }
-// console.log('total', total);
-// !!!!
-// !!!!!!
-// !!!!!!!
-// const numbers = [1, 2, 6, 4, 7, 90, 11, 54, 31, 77, 12, 84, 80, 7, 99, 14];
-// let total = 0;
-
-// for (let i = 0; i < numbers.length; i += 1) {
-//   console.log(numbers[i]);
-
-//   if (numbers[i] % 2 === 0) {
-//     console.log('Четное');
-//   }
-//   total += numbers[i];
-// }
-
-// for (const number of numbers) {
-//   console.log(number);
-
-//   if (number % 2 === 0) {
-//     console.log('Четное');
-//   }
-//   total += number;
-// }
 // !!!!!!!!!
-// !!!!!!!!!
-// !!!!!!!!
-// const logins = [
-//   'Naruto',
-//   'Soro',
-//   'Jack',
-//   'Ludmila',
-//   'Doogee',
-//   'Ezio',
-//   'Thor',
-//   'Me',
-//   'Bona',
-//   'Jack',
-//   'Bono',
-//   'Poly',
-//   'Mango',
-// ];
-// const loginToFind = 'Bono';
-// let message = `Пользователь ${loginToFind} не найден.`;
 
-// for (let i = 0; i < logins.length; i += 1) {
-//   const login = logins[i];
+//                                 УРОК НОМЕР ДВА ОТКРЫТ
 
-//   if (login === loginToFind) {
-//     message = `Пользователь ${loginToFind} найден`;
+// !!!!!!!!!!!!
+
+// const stars = 4;
+// let price;
+
+// switch (stars) {
+//   case 1:
+//     price = `4$`;
 //     break;
-//   }
-// }
-// console.log(message);
-
-// !!
-
-// for (const login of logins) {
-//   console.log(`Login:`, login);
-//   console.log(`${login} === ${loginToFind}: `, login === loginToFind);
-
-//   if (login === loginToFind) {
-//     message = `Пользователь ${loginToFind} найден`;
+//   case 2:
+//     price = `20$`;
 //     break;
-//   }
-// }
+//   case 3:
+//     price = `40$`;
+//     break;
+//   case 4:
+//     price = `220$`;
+//     break;
 
+//   default:
+//     console.log('Много хочешь ');
+// }
+// console.log(price);
+
+// +++++++++++++++++++++++++++=
+
+// const stars = 2;
+// let price;
+
+// switch (stars) {
+//   case 1:
+//   case 2:
+//     price = `4$`;
+//     break;
+
+//   case 3:
+//   case 4:
+//     price = `40$`;
+//     break;
+
+//   case 5:
+//     price = `220$`;
+//     break;
+
+//   default:
+//     console.log('Много хочешь ');
+// }
+// console.log(price);
+
+// !!!!!!!!!!!!
+// !!!!!!!!!!!!!
+
+// const option = 4;
+// let message = '';
+
+// switch (option) {
+//   case 1:
+//     message = 'Вы Можете Забрать Товар из Урны';
+//     break;
+//   case 2:
+//     message = 'Черт Доставит Вам Товар';
+//     break;
+//   case 3:
+//     message = 'Вам отправят товар Укр Почтой';
+//     break;
+//   default:
+//     message = 'Вам перезвонит наш Урод';
+// }
 // console.log(message);
 
-// !!!
-// console.log(logins.includes('Bona'));
-// !!!
-// !!!
-// const message = logins.includes(loginToFind)
-//   ? `Пользователь ${loginToFind} найден.`
-//   : `Пользователь ${loginToFind} не найден. `;
+// !!!!!!!!!!!
+// !!!!!!!!!!!!
+// !!!!!!!!!!!!!!!
 
-// console.log(message);
+// let totalSpent = 3250;
+// let payment = 437;
+// let discount = 0;
 
-// !!!!!!!!!
-// !!!!!!!!!
-// /!!!!!!!!!!!
-
-// const numbers = [43, 50, 33, 69, 2, 11, 9];
-// let smallestNumber = numbers[0];
-
-// for (const number of numbers) {
-//   if (numbers < smallestNumber) {
-//     smallestNumber = number;
-//   }
+// if (totalSpent >= 100 && totalSpent < 1000) {
+//   console.log('Бронзовый партнёр,скидка 2%');
+//   discount = 0.02;
+// } else if (totalSpent >= 1000 && totalSpent < 3000) {
+//   console.log('Серебряный партёр ,скидка 7%');
+//   discount = 0.07;
+// } else if (totalSpent >= 3001) {
+//   console.log('Золотой партёр ,скидка 12%');
+//   discount = 0.12;
+// } else {
+//   console.log('Не партнёр, скидка 0%');
 // }
-// console.log('smallestNumber: ', smallestNumber);
-// !!!!!
-// !!!!!!!
-// !!!!!!!!
-// !!!!!!!!
-// Slice   !!
-// const friends = ['Mango', 'Kiwi', 'Ajax'];
-// let string = '';
 
-// for (const friend of friends) {
-//   string += friend + ',';
-// }
-// string = string.slice(0, string.length - 1);
-// console.log(string);
-// !!!!!
-// !!!!!
-// Join
-// const friends = ['Mango', 'Kiwi', 'Ajax'];
-// const string = friends.join(' ');
-// console.log(string);
+// payment -= payment * discount;
 
-//  !!!!
-// !!!!
-// !!!!!
-// const string = 'JavaScript';
-// const letters = string.split('');
-// let invertedString = '';
+// console.log(
+//   `Оформляем заказ на сумму ${payment} со скидкой ${discount * 100}%`,
+// );
 
-// console.log(letters);
+// totalSpent += payment;
 
-// for (const letter of letters) {
-//   console.log(letter);
+// console.log(`Общая сумма потреченого в магазине ${totalSpent} `);
 
-//   invertedString +=
-//     letter === letter.toLowerCase()
-//       ? letter.toUpperCase()
-//       : letter.toLowerCase();
-// }
-// console.log('invertedString:', invertedString);
-// !!!!!
-// !!!!!
-// 1!!!!!
-// const title = 'Top 10 Benefits of React Framework';
-// const normalizedTitle = title.toLowerCase();
-// const word = normalizedTitle.split(' ');
-// const slug = word.join('-');
-// console.log(slug);
-// !!!!
-// !!!!
-// const title = 'Top 10 Benefits of React Framework';
-// const slug1 = title.toLowerCase().split(' ').join('-');
-// console.log(slug1);
-// !!!!!
-// !!!!!!
-// !!!!!!!
-// const array1 = [5, 10, 20];
-// const array2 = [15, 25, 35];
+// const bthAdd = document.querySelector('button[data-add]');
+// const valueInput = document.querySelector('input[data-value]');
+
 // let total = 0;
 
-// const numbers = array1.concat(array2);
+// bthAdd.addEventListener('click', () => {
+//   console.log('click');
 
-// for (const number of numbers) {
-//   total += number;
-// }
-// console.log(total);
-// !!!!!
-// !!!!!
-// !!!!!!
-// !!!!!!
-// const cards = [
-//   'Карточка-1',
-//   'Карточка-2',
-//   'Карточка-3',
-//   'Карточка-4',
-//   'Карточка-5',
-// ];
-// console.table(cards);
+//   const value = Number(valueInput.value);
+//   console.log(value);
+//   total += value;
+//   console.log('total:', total);
+//   valueInput.value = '';
+// });
 
-// const cardToRemove = 'Карточка-3';
-// const index = cards.indexOf(cardToRemove);
-// console.log(index);
-
-// cards.splice(index, 1);
-
-// console.table(cards);
+// !!!!!!!!!
+// !!!!!!!!
