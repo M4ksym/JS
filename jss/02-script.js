1db721d0824a95bdde03325cb4060e1892d8b0e7
M4ksym <107857845+M4ksym@users.noreply.github.com>
2023-03-22 16:02:16 +0200

    Update 02-script.js

--- jss/02-script.js
+++ jss/02-script.js
@@ -54,54 +54,119 @@
 // !!!!!!!!!!!!
 // !!!!!!!!!!!!!
 
-// const option = 4;
+// const option = 1;
 // let message = '';
 
 // switch (option) {
 //   case 1:
 //     message = 'Вы Можете Забрать Товар из Урны';
 //     break;
+
 //   case 2:
 //     message = 'Черт Доставит Вам Товар';
 //     break;
 //   case 3:
 //     message = 'Вам отправят товар Укр Почтой';
 //     break;
+
 //   default:
 //     message = 'Вам перезвонит наш Урод';
 // }
 // console.log(message);
 
 // !!!!!!!!!!!
-// !!!!!!!!!!!!
-// !!!!!!!!!!!!!!!
+// !!!!!!!!!!!!               ЦЫКЛЫ
 
-// let totalSpent = 3250;
-// let payment = 437;
-// let discount = 0;
-
-// if (totalSpent >= 100 && totalSpent < 1000) {
-//   console.log('Бронзовый партнёр,скидка 2%');
-//   discount = 0.02;
-// } else if (totalSpent >= 1000 && totalSpent < 3000) {
-//   console.log('Серебряный партёр ,скидка 7%');
-//   discount = 0.07;
-// } else if (totalSpent >= 3001) {
-//   console.log('Золотой партёр ,скидка 12%');
-//   discount = 0.12;
-// } else {
-//   console.log('Не партнёр, скидка 0%');
+// for (let i = 0; i <= 7; i += 1) {
+//   console.log(i);
 // }
 
-// payment -= payment * discount;
+// !!!!!!!!!!!!
+// 1!!!!!!!!!!!
 
-// console.log(
-//   `Оформляем заказ на сумму ${payment} со скидкой ${discount * 100}%`,
-// );
+// const employees = 7;
+// const maxSalary = 1200;
+// const minSalary = 100;
+// let totalSalary = 0;
 
-// totalSpent += payment;
+// for (let i = 1; i <= employees; i += 1) {
+//   const salary = Math.round(
+//     Math.random() * (maxSalary - minSalary) + minSalary,
+//   );
+//   console.log(`ЗП работника номер ${i} - ${salary}`);
 
-// console.log(`Общая сумма потреченого в магазине ${totalSpent} `);
+//   totalSalary += salary;
+// }
+// console.log(totalSalary);
+
+// !!!!!!!!!!!!!!!!!!!!!!!
+// !!!!!!!!!!!!!!!!!!!!!!!
+
+// const min = 4;
+// const max = 10;
+// let total = 0;
+
+// for (let i = min; i <= max; i += 1) {
+//   //   console.log(i);
+
+//   if (i % 2 === 0) {
+//     console.log(`четное: `, i);
+//   }
+//   total += i;
+// }
+// console.log('total :', total);
+
+// !!!!!!!!!!!!!!!!!!!!!
+// !!!!!!!!!!!!!!!!!!!!!!
+
+// let balance = 1000;
+// const payment = 200;
+
+// console.log(`Общая сумма заказа ${payment}, баланс на счету`);
+
+// if (balance >= payment) {
+//   console.log(`ок =)`);
+
+//   balance -= payment;
+
+//   console.log(`На вашем счету осталось после покупки ${balance} кредитов`);
+// } else {
+//   console.log('У вас не хватает кредитов =(');
+// }
+
+// !!!!!!!!!!!!!!!!!!!!!
+// !!!!!!!!!!!!!!!!!!!!!
+
+let totalSpent = 3000;
+let payment = 6500;
+let discount = 0;
+
+if (totalSpent >= 100 && totalSpent < 1000) {
+  console.log('Бронзовый партнёр,скидка 2%');
+  discount = 0.02;
+} else if (totalSpent >= 1000 && totalSpent < 3000) {
+  console.log('Серебряный партёр ,скидка 7%');
+  discount = 0.07;
+} else if (totalSpent >= 3000) {
+  console.log('Золотой партёр ,скидка 12%');
+  discount = 0.12;
+} else {
+  console.log('Не партнёр, скидка 0%');
+}
+
+payment -= payment * discount;
+
+console.log(
+  `Оформляем заказ на сумму ${payment} со скидкой ${discount * 100}%`,
+);
+
+totalSpent += payment;
+
+console.log(`Общая сумма потреченого в магазине ${totalSpent} `);
+
+// !!!!!!!!!!!!!!!!!!!!!!
+// 1!!!!!!!!!!!!!!!!!!!!!
+// !!!!!!!!!!!!!!!!!!!!!!
 
 // const bthAdd = document.querySelector('button[data-add]');
 // const valueInput = document.querySelector('input[data-value]');
