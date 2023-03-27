1ce0981fa1356575b897e94c59858b3ccb4ad6b5
M4ksym <107857845+M4ksym@users.noreply.github.com>
2023-03-27 14:02:34 +0300

    Update 085-script.js

--- jss/085-script.js
+++ jss/085-script.js
@@ -535,3 +535,32 @@
 
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = 'jqueryismyjam';
+
+//   if (password === ADMIN_PASSWORD) {
+//     return 'Welcome!';
+//   }
+
+//   return 'Access denied, wrong password!';
+// }
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// 1!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// ?????????????????????????????????????
+
+function checkStorage(available, ordered) {
+  if (ordered === 0) {
+    return 'Your order is empty!';
+  }
+  if (available > ordered) {
+    return 'The order is accepted, our manager will contact you';
+  }
+  if (available < ordered) {
+    return 'Your order is too large, not enough goods in stock!';
+  }
+}
+
+// ??????????????????????????????
