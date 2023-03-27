f67057eada8387e2e7c47f01ab54f7a91cdd0827
M4ksym <107857845+M4ksym@users.noreply.github.com>
2023-03-27 15:34:36 +0300

    Create 086-script.js

---
+++ 086-script.js
@@ -0,0 +1,42 @@
+//
+
+// function checkAge(age) {
+//   if (age >= 18) {
+//     // Change this line
+//     return 'You are an adult';
+//   }
+
+//   return 'You are a minor';
+// }
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = 'jqueryismyjam';
+
+//   if (password === ADMIN_PASSWORD) {
+//     return 'Welcome!';
+//   }
+
+//   return 'Access denied, wrong password!';
+// }
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// 1!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// ?????????????????????????????????????
+
+// function checkStorage(available, ordered) {
+//   if (ordered === 0) {
+//     return 'Your order is empty!';
+//   }
+//   if (available > ordered) {
+//     return 'The order is accepted, our manager will contact you';
+//   }
+//   if (available < ordered) {
+//     return 'Your order is too large, not enough goods in stock!';
+//   }
+// }
+
+// ??????????????????????????????
