cc7155f14e89d4d6ea1f4dd4299d70cb2f436697
M4ksym <107857845+M4ksym@users.noreply.github.com>
2023-03-30 20:57:02 +0300

    Update 086-script.js

--- js/086-script.js
+++ js/086-script.js
@@ -63,7 +63,6 @@
 
 // const fruits = ['apple', 'peach', 'pear', 'banana'];
 // const fruitsArrayLength = fruits.length;
-// // Change code below this line
 
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!
